e9d0d75d47078716809c320cd3435ecfa36b027c
FabriSauce <fabriciosaucedo.fs@gmail.com>
2023-03-09 11:54:05 -0300

    [add: 01-srp.ts]

---
+++ clean-course-main/clean-course-main/src/SOLID/01-srp.ts
@@ -0,0 +1,51 @@
+(() => {
+
+    interface Product { 
+        id:   number;
+        name: string;
+    }
+    
+    // Usualmente, esto es una clase para controlar la vista que es desplegada al usuario
+    // Recuerden que podemos tener muchas vistas que realicen este mismo trabajo.
+    class ProductBloc {
+    
+        loadProduct( id: number ) {
+            // Realiza un proceso para obtener el producto y retornarlo
+            console.log('Producto: ',{ id, name: 'OLED Tv' });
+        }
+    
+        saveProduct( product: Product ) {
+            // Realiza una petición para salvar en base de datos 
+            console.log('Guardando en base de datos', product );
+        }
+    
+        notifyClients() {
+            console.log('Enviando correo a los clientes');
+        }
+    
+    }
+
+    class CartBloc {
+        addToCart( productId: number ) {
+            // Agregar al carrito de compras
+            console.log('Agregando al carrito ', productId );
+        }
+    }
+    
+
+    const cartBloc = new CartBloc();
+    const productBloc = new ProductBloc();
+
+    productBloc.loadProduct(10);
+    productBloc.saveProduct({ id: 10, name: 'OLED TV' });
+    productBloc.notifyClients();
+    cartBloc.addToCart(10);
+
+
+
+
+
+
+
+
+})();
